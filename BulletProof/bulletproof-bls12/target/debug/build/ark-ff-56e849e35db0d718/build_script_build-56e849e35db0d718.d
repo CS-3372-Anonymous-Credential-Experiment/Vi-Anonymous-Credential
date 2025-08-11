f9ee7d84f2e7a81fb2d95b7b258b9b4a42ff6242
DTJ-Tran <Dat.TT228006@sis.hust.edu.vn>
2025-08-11 07:41:13 +0000

    adding the inner_prod_argument

--- BulletProof/bulletproof-bls12/target/debug/build/ark-ff-56e849e35db0d718/build_script_build-56e849e35db0d718.d
+++ BulletProof/bulletproof-bls12/target/debug/build/ark-ff-56e849e35db0d718/build_script_build-56e849e35db0d718.d
@@ -1,5 +1,5 @@
-/home/accumulator/Vi-Anonymous-Credential/BulletProof/bulletproof-bls12/target/debug/build/ark-ff-56e849e35db0d718/build_script_build-56e849e35db0d718.d: /home/accumulator/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ark-ff-0.3.0/build.rs
+/home/accumulator/Vi-Anonymous-Credential/Accumulator/Accumulator_CA/BulletProof/bulletproof-bls12/target/debug/build/ark-ff-56e849e35db0d718/build_script_build-56e849e35db0d718.d: /home/accumulator/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ark-ff-0.3.0/build.rs
 
-/home/accumulator/Vi-Anonymous-Credential/BulletProof/bulletproof-bls12/target/debug/build/ark-ff-56e849e35db0d718/build_script_build-56e849e35db0d718: /home/accumulator/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ark-ff-0.3.0/build.rs
+/home/accumulator/Vi-Anonymous-Credential/Accumulator/Accumulator_CA/BulletProof/bulletproof-bls12/target/debug/build/ark-ff-56e849e35db0d718/build_script_build-56e849e35db0d718: /home/accumulator/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ark-ff-0.3.0/build.rs
 
 /home/accumulator/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ark-ff-0.3.0/build.rs:
--- BulletProof/bulletproof-bls12/target/debug/deps/bulletproofs_bls12-61efe32f659cfaac.d
+++ BulletProof/bulletproof-bls12/target/debug/deps/bulletproofs_bls12-61efe32f659cfaac.d
@@ -1,8 +1,8 @@
-/home/accumulator/Vi-Anonymous-Credential/BulletProof/bulletproof-bls12/target/debug/deps/bulletproofs_bls12-61efe32f659cfaac.d: src/lib.rs src/inner.rs
+/home/accumulator/Vi-Anonymous-Credential/Accumulator/Accumulator_CA/BulletProof/bulletproof-bls12/target/debug/deps/bulletproofs_bls12-61efe32f659cfaac.d: src/lib.rs src/inner.rs
 
-/home/accumulator/Vi-Anonymous-Credential/BulletProof/bulletproof-bls12/target/debug/deps/libbulletproofs_bls12-61efe32f659cfaac.rlib: src/lib.rs src/inner.rs
+/home/accumulator/Vi-Anonymous-Credential/Accumulator/Accumulator_CA/BulletProof/bulletproof-bls12/target/debug/deps/libbulletproofs_bls12-61efe32f659cfaac.rlib: src/lib.rs src/inner.rs
 
-/home/accumulator/Vi-Anonymous-Credential/BulletProof/bulletproof-bls12/target/debug/deps/libbulletproofs_bls12-61efe32f659cfaac.rmeta: src/lib.rs src/inner.rs
+/home/accumulator/Vi-Anonymous-Credential/Accumulator/Accumulator_CA/BulletProof/bulletproof-bls12/target/debug/deps/libbulletproofs_bls12-61efe32f659cfaac.rmeta: src/lib.rs src/inner.rs
 
 src/lib.rs:
 src/inner.rs:
